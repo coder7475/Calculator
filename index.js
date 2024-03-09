0e9cc564758616498287f3fa9e5f9b28417a51d7
coder7475 <robiulhossain7475@gmail.com>
2024-03-09 22:45:14 +0600

    feat: create operator function

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const divide = (num1, num2) => {
     return num1 / num2;
 }
 
-// const operate = (operator, num1, num2) => {
-//     if ()
-// }
+const operate = (operator, num1, num2) => {
+    if (operator === "+")
+        add(num1, num2);
+    else if (operate === '-')
+        subtract(num1, num2)
+    else if (operate === '*')
+        multiply(num1 * num2)
+    else if (operate === "/")
+        divide(num1, num2)
+}
