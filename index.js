d8ef6ea74c08294b339ac447f76457a79ddfa3bb
coder7475 <robiulhossain7475@gmail.com>
2024-03-15 16:16:22 +0600

    feat: select display and digit to show the digit in display

--- index.js
+++ index.js
@@ -24,3 +24,17 @@ const operate = (operator, num1, num2) => {
     else if (operate === "/")
         divide(num1, num2)
 }
+
+// populate the display when clicked button
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.digit');
+
+// console.log(display);
+// console.log(buttons);
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.value;
+        console.log(value);
+    })
+})
